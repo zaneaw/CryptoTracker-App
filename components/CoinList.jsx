fb546262acb9b841ec16773e7c83dae59b3a2e4f
Zane Wilson <wilsonzanea@gmail.com>
2022-11-29 17:03:45 -0500

    cleanup

--- components/CoinList.jsx
+++ components/CoinList.jsx
@@ -1,27 +1,19 @@
 import { View, Text, FlatList } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import CoinListItem from './CoinListItem';
-// import coinData from '../exampleApi';
 
 function CoinList() {
     const [coins, setCoins] = useState([])
 
-
     useEffect(() => {
         getCoins();
-        // setCoins(coinData)
     }, []);
 
     const renderItem = ({ item, index }) => (
         <CoinListItem key={item.id} item={item} index={index} />
     );
 
-    // console.log('coins: ', coins)
-
     const getCoins = () => {
-        // console.log('in getCoins!')
-        // 'https://reactnative.dev/movies.json'
-        // 
         fetch('https://zanes-crypto-tracker-server.cyclic.app/api')
             .then((res) => res.json())
             .then((json) => setCoins(json))
