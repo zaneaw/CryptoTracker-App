2eba8c3cc2fd9e59c965d230cc320f33994c5994
Zane Wilson <wilsonzanea@gmail.com>
2022-12-01 20:28:37 -0500

    safeareaview, cleanup

--- components/Screen.jsx
+++ components/Screen.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { useTheme } from '../theme/ThemeProvider';
 
 export default Screen = props => {
     const { children } = props;
     const { colors, isDark } = useTheme();
 
-    const containerStyle = {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.background,
-    };
-
-    const statusBarStyle = {
-        backgroundColor: colors.primary
-    }
-
     return (
         <>
             <StatusBar animated backgroundColor={colors.background} barStyle={isDark ? 'light-content' : 'dark-content'} />
-            <SafeAreaView style={containerStyle}>
+            <SafeAreaView style={[styles.container, {backgroundColor: colors.background}]}>
                 {children}
             </SafeAreaView>
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
