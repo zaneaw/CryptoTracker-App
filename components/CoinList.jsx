b385f8cdb25e752e164fa4c5a51c6ed3de28050a
Zane Wilson <wilsonzanea@gmail.com>
2022-12-02 18:03:09 -0500

    remove fake api call

--- components/CoinList.jsx
+++ components/CoinList.jsx
@@ -141,34 +141,6 @@ function CoinList() {
         //     .then((res) => res.json())
         //     .then((json) => setCoins(json))
         //     .catch((err) => console.error(err));
-        // setCoins([{
-        //     "id": "bitcoin",
-        //     "symbol": "btc",
-        //     "name": "Bitcoin",
-        //     "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-        //     "current_price": 0.00000932,
-        //     "market_cap": 311637927285,
-        //     "market_cap_rank": 1,
-        //     "fully_diluted_valuation": 340595662445,
-        //     "total_volume": 24666032920,
-        //     "high_24h": 16269.92,
-        //     "low_24h": 15686.55,
-        //     "price_change_24h": 307.8,
-        //     "price_change_percentage_24h": 112.93398,
-        //     "market_cap_change_24h": 5768303762,
-        //     "market_cap_change_percentage_24h": 1.88587,
-        //     "circulating_supply": 19214562,
-        //     "total_supply": 21000000,
-        //     "max_supply": 21000000,
-        //     "ath": 69045,
-        //     "ath_change_percentage": -76.50966,
-        //     "ath_date": "2021-11-10T14:24:11.849Z",
-        //     "atl": 67.81,
-        //     "atl_change_percentage": 23818.43405,
-        //     "atl_date": "2013-07-06T00:00:00.000Z",
-        //     "roi": null,
-        //     "last_updated": "2022-11-23T00:47:57.540Z"
-        // }])
         setTimeout(() => {
             setCoins(coinData);
         }, 50);
