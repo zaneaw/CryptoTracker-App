4ae204148125fbf53265e3eaa725ee4619d64b61
Zane Wilson <wilsonzanea@gmail.com>
2023-01-17 15:15:41 -0500

    setting up Navigation

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ThemeProvider } from './theme/ThemeProvider';
-import Screen from './components/Screen';
+import ScreenWrapper from './components/ScreenWrapper';
 import Main from './components/Main';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
         <ThemeProvider>
-            <Screen>
-                <Main />
-            </Screen>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={ScreenWrapper} />
+                </Stack.Navigator>
+            </NavigationContainer>
         </ThemeProvider>
     );
 };
