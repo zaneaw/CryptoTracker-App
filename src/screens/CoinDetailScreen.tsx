622f9e44592fc852bc2f9005b3a57d0abc0a3397
Zane Wilson <wilsonzanea@gmail.com>
2023-01-27 00:37:17 -0500

    WIP

--- src/screens/CoinDetailScreen.tsx
+++ src/screens/CoinDetailScreen.tsx
@@ -1,21 +1,69 @@
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import {
+    SafeAreaView,
+    View,
+    ScrollView,
+    Text,
+    StyleSheet,
+    ActivityIndicator,
+} from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { useTheme } from '../../theme/ThemeProvider';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import { usePriceChangeOptions } from '../Hooks';
 import { RootStackParamList } from '../routes';
+import { PriceChangePercentageDisplay } from '../components/mini-components';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'CoinDetail'>;
 
 export const CoinDetailScreen: React.FC<Props> = ({ route }) => {
     const { colors } = useTheme();
     const { coinId } = route.params;
+    const [coinData, setCoinData] = useState<any>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const getCoinData = async () => {
+        try {
+            const response = await fetch(
+                `https://zanes-crypto-tracker-server.cyclic.app/api/get-single-coin/${coinId}`,
+            );
+            const json = await response.json();
+            setCoinData(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            return setIsLoading(false)
+        }
+    };
+
+    useEffect(() => {
+        getCoinData();
+    }, []);
 
     return (
         <SafeAreaView
             style={[styles.container, { backgroundColor: colors.background }]}>
-            <View style={[styles.body, { backgroundColor: colors.background }]}>
-                <Text style={{ color: colors.flipText }}>{coinId}</Text>
-            </View>
+            {isLoading ? (
+                <ActivityIndicator size="large" color={colors.primary} />
+            ) : (
+                <ScrollView
+                    style={[
+                        styles.body,
+                        { backgroundColor: colors.background },
+                    ]}>
+                    <View style={styles.topInfo}>
+                        <View style={styles.topLeft}>
+                            <Text style={[styles.headerText, { color: colors.flipText }]}>
+                                {coinData.symbol.toUpperCase()}
+                            </Text>
+                            <Text style={[styles.headerText, {color: colors.flipText}]}>
+                                {new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(coinData.market_data.current_price.usd)}
+                            </Text>
+                        </View>
+                        <PriceChangePercentageDisplay priceChangeAmount={coinData.market_data.price_change_percentage_24h} />
+                    </View>
+                </ScrollView>
+            )}
         </SafeAreaView>
     );
 };
@@ -31,5 +79,16 @@ const styles = StyleSheet.create({
         width: '100%',
         paddingHorizontal: 12,
         paddingVertical: 4,
+    },
+    topInfo: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    topLeft: {},
+    headerText: {
+        fontSize: 16,
+        fontWeight: 'bold',
     },
 });
