e4d658e3bd289dad676b5268b14e9e72272d90c7
Zane Wilson <wilsonzanea@gmail.com>
2023-01-31 17:24:19 -0500

    cleaning up settings page

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -10,10 +10,6 @@ export const SettingsScreen: React.FC = () => {
     return (
         <SafeAreaView
             style={[styles.container, { backgroundColor: colors.background }]}>
-            <View style={[styles.navItemContainer]}>
-                <Text style={{ color: colors.flipText }}>Option 1</Text>
-            </View>
-
             <View style={[styles.darkModeContainer]}>
                 <Text style={{ color: colors.flipText }}>Dark Mode:</Text>
                 <ToggleTheme />
@@ -26,11 +22,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        position: 'relative',
         // margin: 8,
-    },
-    navItemContainer: {
-        marginVertical: 8,
     },
     darkModeContainer: {
         width: '75%',
@@ -39,40 +31,6 @@ const styles = StyleSheet.create({
         alignContent: 'space-around',
         alignItems: 'center',
         alignSelf: 'center',
-        marginVertical: 8,
-        position: 'absolute',
-        bottom: 60,
-    },
-    headerText: {
-        fontSize: 24,
-        position: 'relative',
-    },
-    header: {
-        width: '100%',
-        height: 54,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingVertical: 6,
-        marginBottom: 12,
-    },
-    footer: {
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingVertical: 2,
-        position: 'absolute',
-        bottom: 0,
-    },
-    closeButtonTop: {
-        position: 'absolute',
-        left: 10,
-    },
-    closeButtonBottom: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        //borderWidth: 2,
-        borderRadius: 5,
-        width: '100%',
-        padding: 4,
+        marginVertical: 20,
     },
 });
