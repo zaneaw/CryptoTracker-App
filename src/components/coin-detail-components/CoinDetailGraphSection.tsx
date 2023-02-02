e879f08c0dff179beaaf18cf8cf017e181284dc5
Zane Wilson <wilsonzanea@gmail.com>
2023-02-01 23:23:21 -0500

    graph section of screen

---
+++ src/components/coin-detail-components/CoinDetailGraphSection.tsx
@@ -0,0 +1,71 @@
+import { View } from 'react-native';
+import React, { useEffect, useState, useCallback, Dispatch, SetStateAction } from 'react';
+
+import { CoinDetailGraph } from './CoinDetailGraph';
+import { CoinDetailDays } from './CoinDetailDays';
+
+type Props = {
+    coinId: string;
+    priceChange: number;
+};
+
+export const CoinDetailGraphSection: React.FC<Props> = ({
+    coinId,
+    priceChange,
+}) => {
+    const [amountOfDays, setAmountOfDays] = useState<string>('1');
+    const [graphLabels, setGraphLabels] = useState<string[]>([]);
+    const [graphPoints, setGraphPoints] = useState<number[]>([]);
+
+    const onPressDays = useCallback((apiParam: string): void | Dispatch<SetStateAction<string>> => {
+        if (amountOfDays === apiParam) {
+            return;
+        }
+
+        setAmountOfDays(apiParam);
+    }, []);
+
+    const getCoinGraphData = () => {
+        fetch(
+            `https://zanes-crypto-tracker-server.cyclic.app/api/get-single-coin-ohlc/${coinId}?` + new URLSearchParams({
+                days: amountOfDays,
+            }),
+        )
+            .then(res => res.json())
+            .then(json => cleanGraphData(json))
+            .catch(err => console.error(err));
+    };
+
+    const cleanGraphData = (json: number[][]) => {
+        let currTimestamps = [];
+        let currPointsOpen = [];
+
+        for (let i = 0; i < json.length; i++) {
+            if (Math.floor(json.length / 4) % i === 0) {
+                let date = new Date(json[i][0]);
+                let hours = date.getHours();
+                let minutes = '0' + date.getMinutes();
+                let dateStr = `${hours}:${minutes.substr(-2)}`;
+                currTimestamps.push(dateStr);
+            }
+            currPointsOpen.push(json[i][1]);
+            // graphPointsClose.push(json[i][4]);
+        }
+
+        setGraphLabels(currTimestamps);
+        setGraphPoints(currPointsOpen);
+    };
+
+    useEffect(() => {
+        getCoinGraphData();
+    }, [amountOfDays])
+
+    return (
+        <View style={{marginVertical: 24}}>
+            <CoinDetailDays amountOfDays={amountOfDays} onPressDays={onPressDays} />
+            <CoinDetailGraph graphLabels={graphLabels} graphPoints={graphPoints} priceChange={priceChange} />
+        </View>
+    );
+};
+
+// const styles = StyleSheet.create({});
