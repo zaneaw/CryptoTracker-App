83ea5816bdba9d2c7d329505d4f4ad2761d11a52
Zane Wilson <wilsonzanea@gmail.com>
2023-02-01 23:30:37 -0500

    formatting, remove UStyles

--- src/components/coin-detail-components/CoinDetailGraph.tsx
+++ src/components/coin-detail-components/CoinDetailGraph.tsx
@@ -1,9 +1,9 @@
-import { Dimensions, View } from 'react-native';
 import React from 'react';
+import { Dimensions, View, Text } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
 
-import { useTheme } from '../../../theme/ThemeProvider';
 import { usePriceFormatter } from '../../Hooks';
+import { useTheme } from '../../../theme/ThemeProvider';
 
 type Props = {
     graphLabels: string[];
@@ -11,21 +11,20 @@ type Props = {
     priceChange: number;
 };
 
-export const CoinDetailGraph: React.FC<Props> = ({
-    graphLabels,
-    graphPoints,
-    priceChange,
-}) => {
+export const CoinDetailGraph: React.FC<Props> = ({ graphLabels, graphPoints, priceChange }) => {
     const { colors, isDark } = useTheme();
 
     return (
-        <View>
+        <>
             <LineChart
                 data={{
                     labels: graphLabels,
                     datasets: [
                         {
-                            data: (graphPoints.length !== 0 ? graphPoints : [0, 0, 0, 0, 0, 0, 0, 0]),
+                            data:
+                                graphPoints.length !== 0
+                                    ? graphPoints
+                                    : [0, 0, 0, 0, 0, 0, 0, 0],
                         },
                     ],
                 }}
@@ -41,20 +40,20 @@ export const CoinDetailGraph: React.FC<Props> = ({
                     backgroundGradientFromOpacity: 0,
                     backgroundGradientToOpacity: 0,
                     // decimalPlaces: (graphPoints[graphPoints.length - 1] > 999 ? 0 : 2), // helps with space, not needed now?
-                    color: () => (priceChange >= 0 ? (
-                        isDark ? '#90EE90' : 'green') : (isDark ? '#FFCCCB' : 'red')),
+                    color: () =>
+                        priceChange >= 0
+                            ? isDark
+                                ? '#90EE90'
+                                : 'green'
+                            : isDark
+                            ? '#FFCCCB'
+                            : 'red',
                     labelColor: () => colors.flipText,
                 }}
                 formatYLabel={(yValue: string) => {
                     return usePriceFormatter(yValue, true);
                 }}
-                style={{
-                    marginVertical: 24,
-                    borderRadius: 16,
-                }}
             />
-        </View>
+        </>
     );
 };
-
-// const styles = StyleSheet.create({});
